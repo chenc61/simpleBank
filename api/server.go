54df072d1bfbfa7cebaf89dff41470fdd3d04bc9
chenc <1429278462@qq.com>
2025-09-16 19:59:55 +0800

    feat: 使用gin框架实现几个简单的接口

---
+++ api/server.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	db "simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	/*
+		为什么这块使用指针而不是直接传入对象：
+		1. 避免在函数见传递结构体时进行复制，也可以直接对结构体进行修改
+		2. go一般都使用指针来创建结构体实例
+	*/
+	server := &Server{store: store}
+	/*
+		gin.default做了什么：
+		1. 创建一个gin的引擎实例
+		2. 自动添加中间件：logger（记录http请求日志），recovery（捕捉panic，防止服务器崩溃）
+	*/
+	router := gin.Default()
+
+	// 注册路由： 将POST /accounts请求交给server.createAccount方法处理
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+
+	//数据库驱动程序，需要以副作用的形式导入，并不需要直接使用
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
